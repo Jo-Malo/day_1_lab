8628cf45808362ade1169dc077ee5093600c3837
Jo Malo <juanitamalo.p@gmail.com>
2018-04-16 17:17:10 +0100

    A completed

--- CodeClan_student/students.rb
+++ CodeClan_student/students.rb
@@ -3,6 +3,7 @@ class CodeclanStudent
   def initialize(input_name, input_cohort)
     @student_name = input_name
     @cohort_number = input_cohort
+    @favourite_lang = input_lang
   end
 
   def get_student_name()
@@ -10,7 +11,19 @@ class CodeclanStudent
   end
 
   def get_cohort_number()
+    return @cohort_number
+  end
 
+  def set_student_name(name)
+    @student_name = name
+  end
+
+  def set_cohort_number(number)
+    @cohort_number = number
+  end
+
+  def student_talk()
+    return "i love ruby"
   end
 
 end
--- CodeClan_student/students_specs.rb/codeclan_student_specs.rb
+++ CodeClan_student/students_specs.rb/codeclan_student_specs.rb
@@ -14,4 +14,21 @@ class TestCodeclanStudent < MiniTest::Test
     assert_equal(10, codeclan_student.get_cohort_number())
   end
 
+  def test_set_student_name
+    codeclan_student = CodeclanStudent.new("Rambo", 10)
+    codeclan_student.set_student_name("John")
+    assert_equal("John", codeclan_student.get_student_name())
+  end
+
+  def test_set_cohort_number
+    codeclan_student = CodeclanStudent.new("Rambo", 10)
+    codeclan_student.set_cohort_number(21)
+    assert_equal(21, codeclan_student.get_cohort_number())
+  end
+
+  def test_student_talk
+    codeclan_student = CodeclanStudent.new("Rambo", 10)
+    assert_equal("i love ruby", codeclan_student.student_talk())
+  end
+
 end
